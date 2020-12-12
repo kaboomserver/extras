ae0e7d61eecfd80da5cecbd2c438dba618a80e81
Apmunute <munuteaugusto@gmail.com>
2020-12-12 14:00:16 +0000

    fix forceload on execute

--- src/main/java/pw/kaboom/extras/modules/server/ServerCommand.java
+++ src/main/java/pw/kaboom/extras/modules/server/ServerCommand.java
@@ -29,7 +29,8 @@ public final class ServerCommand implements Listener {
 								if (i + 1 < arr.length) {
 									if ("execute".equalsIgnoreCase(arr[i + 1])
 											|| "clone".equalsIgnoreCase(arr[i + 1])
-											|| "fill".equalsIgnoreCase(arr[i + 1])
+                                            || "fill".equalsIgnoreCase(arr[i + 1])
+                                            || "forceload".equalsIgnoreCase(arr[i + 1])
 											|| "me".equalsIgnoreCase(arr[i + 1])
 											|| "msg".equalsIgnoreCase(arr[i + 1])
 											|| "particle".equalsIgnoreCase(arr[i + 1])
