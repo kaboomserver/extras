ff877e136f6721ce6ad1d78a424fdae52163a56b
Businses Goose <arclicious@vivaldi.net>
2021-03-19 21:02:48 +0000

    Patch integers of value 20 million and up in commands

--- src/main/java/pw/kaboom/extras/modules/server/ServerCommand.java
+++ src/main/java/pw/kaboom/extras/modules/server/ServerCommand.java
@@ -45,6 +45,25 @@ public final class ServerCommand implements Listener {
 			commandName = "/" + arr[0].toLowerCase();
 		}
 
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i].matches("^[+-]?(?:\\d+\\.?\\d*|\\d*\\.?\\d+)[\\r\\n]*$")) {
+				try {
+					int integer = Integer.parseInt(arr[i]);
+					try {
+						if (integer == Double.MAX_VALUE || integer == Float.MAX_VALUE || integer == Integer.MAX_VALUE) {
+							return "cancel";
+						}
+					} catch (Exception e) {
+						return "cancel";
+					}
+
+				} catch (NumberFormatException e) {
+					// Ignore exception
+				}
+
+			}
+		}
+
 		try {
 			switch (commandName) {
 				case "/minecraft:execute":
