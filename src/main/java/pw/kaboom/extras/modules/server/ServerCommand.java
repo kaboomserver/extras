634661e1fa199f174d98f48e12d9770af7483f37
Allink <arclicious@vivaldi.net>
2023-04-01 23:46:17 +0100

    Improve distance selector detection

--- src/main/java/pw/kaboom/extras/modules/server/ServerCommand.java
+++ src/main/java/pw/kaboom/extras/modules/server/ServerCommand.java
@@ -19,6 +19,8 @@ import java.util.regex.Pattern;
 public final class ServerCommand implements Listener {
     private static final Pattern AS_AT_PATTERN = Pattern.compile(
         "\\b(as|at|facing entity) @[ae]\\b");
+    private static final Pattern DISTANCE_SELECTOR_PATTERN = Pattern.compile(
+            "[\"']?distance[\"']?\\s*=\\s*");
     private static final Logger LOGGER = JavaPlugin.getPlugin(Main.class).getLogger();
 
     public static boolean checkExecuteCommand(final String cmd) {
@@ -208,8 +210,8 @@ public final class ServerCommand implements Listener {
         }
 
         if (command.contains("distance")) {
-            return command.replace("distance=", "]").replace("\"distance\"=", "]")
-                .replace("'distance'=", "]");
+            final Matcher distanceMatcher = DISTANCE_SELECTOR_PATTERN.matcher(command);
+            return distanceMatcher.replaceAll("]");
         }
 
         return null;
