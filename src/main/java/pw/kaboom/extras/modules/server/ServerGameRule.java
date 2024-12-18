e4000b8881a6271c1bd605414e6e67a7c588da6a
amyavi <144570677+amyavi@users.noreply.github.com>
2024-12-18 16:02:17 -0300

    feat: use maxCommandForkCount gamerule for limiting execute
    refactor: clean up gamerule limiter

--- src/main/java/pw/kaboom/extras/modules/server/ServerGameRule.java
+++ src/main/java/pw/kaboom/extras/modules/server/ServerGameRule.java
@@ -8,31 +8,59 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.scheduler.BukkitScheduler;
 import pw.kaboom.extras.Main;
+import pw.kaboom.extras.modules.entity.EntitySpawn;
+
+import java.util.Map;
 
 public final class ServerGameRule implements Listener {
+    private static final Map<GameRule<Integer>, Integer> GAMERULE_LIMITS = Map.of(
+            GameRule.RANDOM_TICK_SPEED, 6,
+            GameRule.SPAWN_RADIUS, 100,
+            GameRule.COMMAND_MODIFICATION_BLOCK_LIMIT, 32768,
+            GameRule.MAX_COMMAND_FORK_COUNT, EntitySpawn.MAX_ENTITIES_PER_WORLD
+    );
+
     @EventHandler
     void onGameRuleChange(final WorldGameRuleChangeEvent event) {
         final GameRule<?> gameRule = event.getGameRule();
 
-        if ((gameRule == GameRule.RANDOM_TICK_SPEED
-                && Integer.parseInt(event.getValue()) > 6)
-            || (event.getGameRule() == GameRule.SPAWN_RADIUS
-                && Integer.parseInt(event.getValue()) > 100)
-            || (event.getGameRule() == GameRule.COMMAND_MODIFICATION_BLOCK_LIMIT
-                && Integer.parseInt(event.getValue()) > 32768)) {
-            event.setCancelled(true);
+        final Integer limit = GAMERULE_LIMITS.get(gameRule);
+        if (limit == null) {
+            return;
+        }
+
+        final int value = Integer.parseInt(event.getValue());
+        if (value > limit) {
+            event.setValue(limit.toString());
         }
     }
 
     private static void enableAutoSave() {
-        for (final World world: Bukkit.getWorlds()) {
+        for (final World world : Bukkit.getWorlds()) {
             world.setAutoSave(true);
+        }
+    }
+
+    private static void fixGameRules() {
+        for (final World world : Bukkit.getWorlds()) {
+            for (final var entry : GAMERULE_LIMITS.entrySet()) {
+                final GameRule<Integer> gameRule = entry.getKey();
+                final int limit = entry.getValue();
+
+                final Integer value = world.getGameRuleValue(gameRule) != null
+                        ? world.getGameRuleValue(gameRule) : world.getGameRuleDefault(gameRule);
+
+                if (value == null || value > limit) {
+                    world.setGameRule(gameRule, limit);
+                }
+            }
         }
     }
 
     public static void init(final Main main) {
         final BukkitScheduler scheduler = Bukkit.getScheduler();
 
+        scheduler.runTask(main, ServerGameRule::fixGameRules); // Right before server fully starts
         scheduler.runTaskTimer(main, ServerGameRule::enableAutoSave, 0L, 600L); // 30 seconds
     }
 }
