4d398041e294616759beca456e4e93a6c2647e30
opt <173472493+OptimisticDeving@users.noreply.github.com>
2026-05-27 18:50:40 +0100

    Replace control characters when logging console commands (#391)
    Prevents people from using ANSI escape sequences and messing up the terminal, among other undesirable sequences (i.e. RLO, newline, carriage return), without mangling emoji & non-English text.
    
    Co-authored-by: Kaboom <58372747+kaboombot@users.noreply.github.com>

--- src/main/java/pw/kaboom/extras/modules/server/ServerCommand.java
+++ src/main/java/pw/kaboom/extras/modules/server/ServerCommand.java
@@ -20,6 +20,7 @@ import java.util.regex.Pattern;
 
 public final class ServerCommand implements Listener {
     private static final Pattern SELECTOR_PATTERN = Pattern.compile("(?>\\s)*@[aenprs](?>\\s)*");
+    private static final Pattern CONTROL_CHAR_PATTERN = Pattern.compile("\\p{C}");
     private static final Logger LOGGER = Main.PLUGIN.getLogger();
 
     private static final Set<String> BLOCKED_EXECUTE_COMMANDS = ImmutableSet.of(
@@ -199,6 +200,9 @@ public final class ServerCommand implements Listener {
             }
         }
 
-        LOGGER.log(Level.INFO, "Console command: " + command);
+        LOGGER.log(
+                Level.INFO,
+                "Console command: " + CONTROL_CHAR_PATTERN.matcher(command).replaceAll("\ufffd")
+        );
     }
 }
